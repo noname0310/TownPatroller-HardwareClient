cbee8c8e31f1e4722a18035ca4987ef74375d3a9
noname <48761044+noname0310@users.noreply.github.com>
2019-11-08 20:36:35 +0900

    clientid ulong 지원

--- TownPatroller/Assets/Scripts/Socket/Helper/TestHelper.cs
+++ TownPatroller/Assets/Scripts/Socket/Helper/TestHelper.cs
@@ -13,6 +13,6 @@ public class TestHelper : MonoBehaviour
     {
         IP.text = "noname0310.iptime.org";
         Port.text = "20310";
-        ID.text = Random.Range(10000000, 99999999).ToString();
+        ID.text = ((ulong)Random.Range(10000000000000000000, ulong.MaxValue)).ToString();
     }
 }
